d35d7f36da342745d52098856e689195f980c326
surface@pro.3 <348699103@qq.com>
2020-11-13 03:15:42 +0800

    修正了调用参数

--- youtube-downloader.py
+++ youtube-downloader.py
@@ -20,7 +20,7 @@ class YoutubeDownload:
         print(template.format(fore_color=palette[id % 4], content=content))
 
     def the_guide(self):
-        self.prompt = '（当前Mp4转换状态：{recode_video_status}）（输入"mp4"切换状态）输入指令或Url：'
+        self.prompt = '(当前Mp4转换状态：{recode_video_status})(输入"mp4"切换状态) 输入指令或Url：'
         self.prompt = self.prompt.format(recode_video_status=self.recode_video_sign)
         if self.recode_video_sign:
             self.download_command = self.download_command_template.replace('#ecode-video-placeholder#', '--recode-video mp4')
@@ -39,24 +39,26 @@ class YoutubeDownload:
     def download_dispatcher(self, url):
         self.serialno += 1
         id = self.serialno
-        print('开始下载：', url)
+        self.colored_print('开始下载：' + url, id)
         temp_folder = str(time.time()).replace('.', '')
         download_command = self.download_command.format(temp_folder=temp_folder, url=url)
-        print('下载指令：', download_command)
-        retry_times, success_sign = 5, False
+        self.colored_print('下载指令：' + download_command, id)
+        retry_times, success_sign = 10, False
         while retry_times >= 0:
             if self.download_process(download_command, id):
                 success_sign = True
                 break
             else:
                 retry_times -= 1
+                time.sleep(10)
+                self.colored_print(f'重试，剩余{retry_times}次机会...', id)
         if success_sign:
             for file_ in os.listdir(temp_folder):
                 shutil.move(os.path.join(temp_folder, file_), self.target_folder_path)
             shutil.rmtree(temp_folder)
-            print('下载完成！', url)
+            self.colored_print('下载完成！' + url, id)
         else:
-            print('下载失败！', url)
+            self.colored_print('下载失败！' + url, id)
 
     def run_local_loop(self):
         while True:
